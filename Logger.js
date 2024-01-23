e9e4acacedf0d0426cf9599c7854a5af7c108bbe
Qianwen Lyu <qianwenlyu@Qianwens-MacBook-Pro.local>
2024-01-23 22:46:30 +0000

    add states log

--- Logger.js
+++ Logger.js
@@ -2,11 +2,15 @@ const fs = require('node:fs')
 
 var Logger = {
     logQueue: [],
-
+    statesLogQueue: [],
 }
 
 Logger.info = function(infoStr){
     this.logQueue.push(infoStr)
+}
+
+Logger.statesInfo = function(infoStr){
+    this.statesLogQueue.push(infoStr)
 }
 
 Logger.writeToFile = function(){
@@ -23,10 +27,25 @@ Logger.writeToFile = function(){
             Logger.clearQueue()
         }
     }) 
+
+    var statesContent = ""
+    for (let i = 0; i < Logger.statesLogQueue.length; i++){
+        statesContent = statesContent + Logger.statesLogQueue[i] + "\n"
+    }
+
+    fs.writeFileSync('./StatesLog.txt', statesContent, (err) => { 
+        // In case of a error throw err. 
+        if (err) throw err;
+        else {
+            console.log('successful')
+            Logger.clearQueue()
+        }
+    }) 
 }
 
 Logger.clearQueue = function(){
     Logger.logQueue = []
+    Logger.statesLogQueue = []
 }
 
 module.exports = {
