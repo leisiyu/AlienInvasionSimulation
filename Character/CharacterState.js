86f6add7c6ccf9666ed5fabbd4a67f397268abd0
Qianwen Lyu <qianwenlyu@Qianwens-MacBook-Pro.local>
2024-02-16 23:35:51 +0000

    add CharacterState

---
+++ Character/CharacterState.js
@@ -0,0 +1,25 @@
+const Utils = require('../Utils.js') 
+
+class CharacterState{
+    stateType = ""
+    target = {}
+    constructor(){
+        this.stateType = Utils.CHARACTER_STATES.PATROL
+    }
+
+    setState(stateType, target){
+        this.stateType = stateType
+        this.target = target
+
+        console.log("state updated!")
+    }
+
+    // stateFinished(){
+    //     this.stateType = Utils.CHARACTER_MISSION.PATROL
+    //     this.target = {}
+    // }
+}
+
+module.exports = {
+    CharacterState,
+}
