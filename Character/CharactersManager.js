cf738e8fcd4dd98fb1e522715e1778da0cebe60d
Qianwen Lyu <qianwenlyu@Qianwens-MacBook-Pro.local>
2025-11-12 23:17:08 +0000

    amend: agents acts interval

--- Character/CharactersManager.js
+++ Character/CharactersManager.js
@@ -16,14 +16,8 @@ function generateAllCharacters(){
 		var pos = TownMap.getInstance().generateRandomPos();
 		// var randomName = utils.TOWNFOLK_NAMES[Math.floor(Math.random() * utils.TOWNFOLK_NAMES.length)];
 		var townfolkName = "t" + (i + 1)
-		// Logger.statesInfo(JSON.stringify({
-		// 	N: townfolkName,
-		// 	A: "m", 
-		// 	P: pos,
-		// 	T: 0,
-		// }))
 		var townfolk = new Townfolk(townfolkName, pos);
-		Scheduler.scheduler.scheduleRepeatingIn(townfolk.simEvent, 5)
+		Scheduler.scheduler.scheduleRepeatingIn(townfolk.simEvent, 1)
 		CharactersData.charactersArray.push(townfolk)
 	}
 	for (let i = 0; i < utils.SOLIDERS_NUM; i++) {
@@ -31,13 +25,7 @@ function generateAllCharacters(){
 		// var randomName = utils.TOWNFOLK_NAMES[Math.floor(Math.random() * utils.TOWNFOLK_NAMES.length)];
 		var soldierName = "s" + (i + 1)
 		var soldier = new Soldier(soldierName, pos);
-		// Logger.statesInfo(JSON.stringify({
-		// 	N: soldierName,
-		// 	A: "m", 
-		// 	P: pos,
-		// 	T: 0,
-		// }))
-		Scheduler.scheduler.scheduleRepeatingIn(soldier.simEvent, 5)
+		Scheduler.scheduler.scheduleRepeatingIn(soldier.simEvent, 1)
 		CharactersData.charactersArray.push(soldier)
 	}
 	for (let i = 0; i < utils.ALIENS_NUM; i++) {
@@ -45,13 +33,7 @@ function generateAllCharacters(){
 		// var randomName = utils.TOWNFOLK_NAMES[Math.floor(Math.random() * utils.TOWNFOLK_NAMES.length)];
 		var alienName = "a" + (i + 1)
 		var alien = new Alien(alienName, pos);
-		// Logger.statesInfo(JSON.stringify({
-		// 	N: alienName,
-		// 	A: "m", 
-		// 	P: pos,
-		// 	T: 0,
-		// }))
-		Scheduler.scheduler.scheduleRepeatingIn(alien.simEvent, 5)
+		Scheduler.scheduler.scheduleRepeatingIn(alien.simEvent, 1)
 		CharactersData.charactersArray.push(alien)
 	}
 }
